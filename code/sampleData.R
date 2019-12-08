67fda7ce90e7fc057e07707d36f91e2f7bbd614a
nilseling <nils.eling@gmx.de>
2019-12-08 11:11:20 +0100

    File to produce sample data

---
+++ code/sampleData.R
@@ -0,0 +1,70 @@
+######################################################################
+#### This script serves the purpose to prepare a sample dataset   ####
+#### in the format of the 'cpout' folder of the IMC segmentation  ####
+#### pipeline. For now, we will use altered data from the IMMUCan ####
+#### project                                                      ####
+######################################################################
+
+#############################
+# 1. Prepare cells.csv file #
+#############################
+
+# Read in data
+cells <- read.csv("/Volumes/immucan_volume/processed_data/Panel_1/Batch20191023/IMMUcan_Batch20191023_panel_1_1_1.05/cpout/cell.csv")
+
+# Subset data
+# Here, I will select a sample with very small images.
+cells <- cells[cells$ImageNumber %in% c(16, 17, 18, 19),]
+table(cells$ImageNumber)
+
+# Change image numbering
+cells[cells$ImageNumber == 16,"ImageNumber"] <- 1
+cells[cells$ImageNumber == 17,"ImageNumber"] <- 2
+cells[cells$ImageNumber == 18,"ImageNumber"] <- 3
+cells[cells$ImageNumber == 19,"ImageNumber"] <- 4
+
+###################################
+# 2. Prepare Object relationships #
+###################################
+
+relationships <- read.csv("/Volumes/immucan_volume/processed_data/Panel_1/Batch20191023/IMMUcan_Batch20191023_panel_1_1_1.05/cpout/Object relationships.csv")
+
+# Subset data
+relationships <- relationships[relationships$First.Image.Number %in% c(16, 17, 18, 19),]
+
+# Rename images
+relationships[relationships$First.Image.Number == 16,"First.Image.Number"] <- 1
+relationships[relationships$First.Image.Number == 17,"First.Image.Number"] <- 2
+relationships[relationships$First.Image.Number == 18,"First.Image.Number"] <- 3
+relationships[relationships$First.Image.Number == 19,"First.Image.Number"] <- 4
+
+relationships[relationships$Second.Image.Number == 16,"Second.Image.Number"] <- 1
+relationships[relationships$Second.Image.Number == 17,"Second.Image.Number"] <- 2
+relationships[relationships$Second.Image.Number == 18,"Second.Image.Number"] <- 3
+relationships[relationships$Second.Image.Number == 19,"Second.Image.Number"] <- 4
+
+
+##################################
+# 3. Prepare Image metadata file #
+##################################
+
+image <- read.csv("/Volumes/immucan_volume/processed_data/Panel_1/Batch20191023/IMMUcan_Batch20191023_panel_1_1_1.05/cpout/Image_2.csv")
+
+# Subset data
+image <- image[c(16,17,18,19),]
+image$ImageNumber <- c(1,2,3,4)
+
+####################
+# 4. Prepare panel #
+####################
+
+panel <- read.csv("/Volumes/immucan_volume/processed_data/Panel_1/Batch20191023/20191101_IMMUcan_panel_1_1.05.csv")
+
+######################
+# 5. Write out files #
+######################
+
+write.csv(cells, "data/extdata/cells.csv", row.names = FALSE)
+write.csv(relationships, "data/extdata/Object relationships.csv", row.names = FALSE)
+write.csv(image, "data/extdata/image.csv", row.names = FALSE)
+write.csv(panel, "data/extdata/panel.csv", row.names = FALSE)
